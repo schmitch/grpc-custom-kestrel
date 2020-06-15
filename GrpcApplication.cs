81cb32e52f97e1074ebabbe718904059c73a69cc
Christian Schmitt <c.schmitt@envisia.de>
2020-06-15 10:15:05 +0200

    made it working

--- GrpcApplication.cs
+++ GrpcApplication.cs
@@ -3,33 +3,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace WorkerWebProject
 {
     public class GrpcApplication : IHttpApplication<HttpContext>
     {
         private readonly RequestDelegate _request;
-        private readonly IServiceProvider _provider;
+        private readonly IHttpContextFactory _httpContextFactory;
 
         public GrpcApplication(RequestDelegate request, IServiceProvider provider)
         {
             _request = request;
-            _provider = provider;
+            _httpContextFactory = new DefaultHttpContextFactory(provider);
         }
 
         public HttpContext CreateContext(IFeatureCollection contextFeatures)
         {
-            var scopeFactory = _provider.GetRequiredService<IServiceScopeFactory>();
-            using var scope = scopeFactory.CreateScope();
-            return scope.ServiceProvider.GetRequiredService<IHttpContextFactory>().Create(contextFeatures);
+            return _httpContextFactory.Create(contextFeatures);
         }
 
         public void DisposeContext(HttpContext context, Exception exception)
         {
-            var scopeFactory = _provider.GetRequiredService<IServiceScopeFactory>();
-            using var scope = scopeFactory.CreateScope();
-            scope.ServiceProvider.GetRequiredService<IHttpContextFactory>().Dispose(context);
+            _httpContextFactory.Dispose(context);
         }
 
         public async Task ProcessRequestAsync(HttpContext context)
